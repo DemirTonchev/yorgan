a6326fe4a04f56e9afdef6915752de3ffa9a08de
asbikov <asbikov@gmail.com>
2025-11-28 11:41:28 +0200

    allow origin 127.0.0.1

--- app/main.py
+++ app/main.py
@@ -110,7 +110,7 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
