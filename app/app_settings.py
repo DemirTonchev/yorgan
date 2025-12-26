fea818938b62738e9b9de1134eb40c00350b3eb6
asbikov <asbikov@gmail.com>
2025-12-26 20:13:43 +0200

    remove legacy settings

--- app/app_settings.py
+++ app/app_settings.py
@@ -1,23 +1,14 @@
-from enum import StrEnum
-from typing import Optional
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
-
-# ParseServiceOptions = StrEnum("ParseServiceOptions", "gemini gpt landingai")
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_ignore_empty=True,  # use the default values instead of emtpy env var in case it is not configured in .env
     )
-    parse_llm: str = "gemini-2.5-flash"
-    # parse_llms: dict[str, str] = "gemini-2.5-flash"
-    # gpt_parse_llm: Optional[str] = "gpt-4.1"
-    # extract_llm: str = "gemini-2.5-flash"
-
-    # parse_service_backend: str = "landingai"
+    version: str = "0.1.0"
 
 
 settings = Settings()
